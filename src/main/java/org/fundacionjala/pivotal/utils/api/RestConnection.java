d8374741a069c48b9d4786ed4470e16c58569db9
daniel-m10 <daniel.montecinos@fundacion-jala.org>
2017-06-01 18:42:15 -0400

    RequestManager and RequestConnection classes (#4)

---
+++ src/main/java/org/fundacionjala/pivotal/utils/api/RestConnection.java
@@ -0,0 +1,50 @@
+package org.fundacionjala.pivotal.utils.api;
+
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import org.fundacionjala.pivotal.utils.Environment;
+
+/**
+ * Created by Administrator on 5/25/2017.
+ */
+public final class RestConnection {
+    private static int proxyPort = Integer.parseInt(Environment.getInstance()
+            .getPropertyValue("testerAt03.proxyPort"));
+    private static String proxyIp = Environment.getInstance()
+            .getPropertyValue("testerAt03.proxyIp");
+    private static String apiToken = Environment.getInstance()
+            .getPropertyValue("testerAt03.apiToken");
+    private static String baseURI = Environment.getInstance()
+            .getPropertyValue("testerAt03.baseURI");
+    private static RestConnection instance;
+    private RequestSpecification requestSpecification;
+
+    /**
+     * The constructor constructs an specific request, using all the next
+     * common parameters: proxy, header and baseUri.
+     */
+    private RestConnection() {
+        requestSpecification = new RequestSpecBuilder()
+                .setProxy(proxyIp, proxyPort)
+                .addHeader("X-TrackerToken", apiToken)
+                .setBaseUri(baseURI)
+                .build();
+    }
+
+    /**
+     * @return the RestConnection instance.
+     */
+    public static RestConnection getInstance() {
+        if (instance == null) {
+            instance = new RestConnection();
+        }
+        return instance;
+    }
+
+    /**
+     * @return a request specification.
+     */
+    public RequestSpecification getRequestSpecification() {
+        return requestSpecification;
+    }
+}
